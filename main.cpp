a75b69ad308b4e34c265a722e39896b3b1b6bf75
oranmehavi <oranhero@gmail.com>
2024-04-09 22:32:52 +0300

    Check if running as root.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,17 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <unistd.h>
 #include "configuration.h"
 #include "downloaderinstaller.h"
 
 int main(int argc, char *argv[])
 {
+
+    if (geteuid() == 0) {
+        qInfo() << "Running as root.";
+        return 1;
+    }
+
     QGuiApplication app(argc, argv);
     Configuration* config = new Configuration(&app);
     DownloaderInstaller* downloaderInstaller = new DownloaderInstaller(&app);
